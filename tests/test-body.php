028983ee891682947f88959d9dc3ab1db6554140
Marko Jakic <jakicmeister@gmail.com>
2015-02-28 23:50:40 +0100

    Set WOR_SITE variable for home url

--- tests/test-body.php
+++ tests/test-body.php
@@ -3,20 +3,33 @@
 class BodyTest extends WP_UnitTestCase {
 
   function testBodies() {
-    $body = wp_remote_retrieve_body(wp_remote_get('http://wor.dev/foo/bar'));
+    $home_url = getenv('WOR_SITE');
+
+    if (!$home_url) {
+      $this->print_wor_site_warning();
+      return;
+    }
+
+    $body = wp_remote_retrieve_body(wp_remote_get("{$home_url}/foo/bar"));
     $this->assertEquals('Hello Baz!', $body);
 
     // METHOD: POST
-    $body = wp_remote_retrieve_body(wp_remote_post('http://wor.dev/foo/bar'));
+    $body = wp_remote_retrieve_body(wp_remote_post("{$home_url}/foo/bar"));
     $this->assertEquals('Hello Baz!', $body);
 
     // $_GET['param'] = 'keyboard'
-    $body = wp_remote_retrieve_body(wp_remote_get('http://wor.dev/foo/bar/keyboard'));
+    $body = wp_remote_retrieve_body(wp_remote_get("{$home_url}/foo/bar/keyboard"));
     $this->assertEquals('Param: keyboard', $body);
 
     // $_GET['param1'] = 'keyboard', $_GET['param2'] = 'mouse'
-    $body = wp_remote_retrieve_body(wp_remote_get('http://wor.dev/foo/bar/keyboard/mouse'));
+    $body = wp_remote_retrieve_body(wp_remote_get("{$home_url}/foo/bar/keyboard/mouse"));
     $this->assertEquals('Params: keyboard, mouse', $body);
+  }
+
+
+  function print_wor_site_warning() {
+    print(PHP_EOL . '**' . PHP_EOL . 'You should export WOR_SITE variable first.'
+      . PHP_EOL . '**' . PHP_EOL);
   }
 }
 
